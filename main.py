24469a6f00e40fd288829efa7f72d360c966a0b5
leeny_the_bear <leeny@inbox.ru>
2022-11-02 20:12:17 +0600

    add exceptions catching

--- main.py
+++ main.py
@@ -2,14 +2,21 @@ import os
 from dotenv import load_dotenv
 
 import requests
-
+from requests import ReadTimeout, ConnectionError
 
 url = 'https://dvmn.org/api/user_reviews/'
 load_dotenv()
 token = os.environ['TOKEN']
 
+while True:
+    headers = {'Authorization': f'Token {token}'}
+    try:
+        response = requests.get(url_for_long_polling, headers=headers, timeout=5)
+        response.raise_for_status()
+        print(response.text)
+    except ReadTimeout:
+        continue
+    except ConnectionError:
+        continue
 
-headers = {'Authorization': f'Token {token}'}
-response = requests.get(url, headers=headers)
-response.raise_for_status()
-print(response.text)
+

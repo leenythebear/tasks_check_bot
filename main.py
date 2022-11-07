003a9ace08d8e3dff501299f54067b8dc323afb4
leeny_the_bear <leeny@inbox.ru>
2022-11-07 14:51:23 +0600

    make a func for long polling requests

--- main.py
+++ main.py
@@ -7,17 +7,39 @@ from requests import ReadTimeout, ConnectionError
 url = 'https://dvmn.org/api/user_reviews/'
 url_for_long_polling = 'https://dvmn.org/api/long_polling/'
 load_dotenv()
-token = os.environ['TOKEN']
 
-while True:
+
+def send_requests(token):
     headers = {'Authorization': f'Token {token}'}
-    try:
-        response = requests.get(url_for_long_polling, headers=headers, timeout=5)
-        response.raise_for_status()
-        print(response.text)
-    except ReadTimeout:
-        continue
-    except ConnectionError:
-        continue
+    timestamp = time.time()
+    while True:
+        try:
+            response = requests.get(url_for_long_polling, headers=headers, params={'timestamp': timestamp}, timeout=90)
+            response.raise_for_status()
+            timestamp = response.json()['last_attempt_timestamp']
+            # response = requests.get(url_for_long_polling, headers=headers, , timeout=5)
+            # response.raise_for_status()
+            print(response.text)
+        except ReadTimeout:
+            continue
+        except ConnectionError:
+            continue
+        time.sleep(90)
+
+
+def
+
+if __name__ == '__main__':
+    load_dotenv()
+
+    dvmn_token = os.environ['DVMN_TOKEN']
+    bot_token = os.environ['BOT_TOKEN']
+    chat_id = os.environ['CHAT_ID']
+
+
+
+
+
+
 
 
